d79a893130a773c992cd525c702d8f937308cce8
dkawula <dkawula@triconelite.com>
2017-11-29 10:35:24 -0700

    Factory Reset to clear the Networking Configurations
    This allows us to redo the Networking Configurations for DCB and RDMA
    --> I use this a lot for demo purposes showing how to configure nodes.

---
+++ S2D/FactoryReset-Clear-NetworkingConfigs.ps1
@@ -0,0 +1,13 @@
+﻿#region 023 - Optional Physical LAB RESET ###WARNING WILL WIPE ALL CLUSTER DISKS AND DATA #### Wiping the NIC Configuraiton to start the Demo Over
+
+$Nodes = ('S2D1','S2D2')
+Invoke-Command $Nodes {C:\post-install\Clear-SdsConfig.ps1 -confirm:$False}
+Enter-PSSession -ComputerName S2D2
+
+Invoke-Command $Nodes {
+#DO NOT RUN THIS COMMAND IN PROD!!!
+Get-VMNetworkAdapter -ManagementOS | Where-Object {$_.Name -ne 'Production'} | Remove-VMNetworkAdapter -confirm:$False
+Get-VmSwitch | Where-Object {$_.Name -ne 'Embedded_vSwitch_Team_Production'} | Remove-Vmswitch -Confirm:$False
+Get-NetQosPolicy | Remove-NetQosPolicy -confirm:$Falase
+Get-NetQosTrafficClass | Remove-NetQosTrafficClass
+#Clear-ClusterNode
