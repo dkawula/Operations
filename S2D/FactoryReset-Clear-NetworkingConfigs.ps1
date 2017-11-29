40156fa5d4d43207ee0a5f0f442c123947f4074f
dkawula <dkawula@triconelite.com>
2017-11-29 10:37:17 -0700

    Networking Factory Reset for S2D
    I use this to reset the DCB and RDMA Configurations in my lab and at
    customers sites if they want to redo things.

--- S2D/FactoryReset-Clear-NetworkingConfigs.ps1
+++ S2D/FactoryReset-Clear-NetworkingConfigs.ps1
@@ -1,8 +1,8 @@
-﻿#region 023 - Optional Physical LAB RESET ###WARNING WILL WIPE ALL CLUSTER DISKS AND DATA #### Wiping the NIC Configuraiton to start the Demo Over
+﻿#region Factory Reset the Networking Configuraitons on all nodes
 
 $Nodes = ('S2D1','S2D2')
-Invoke-Command $Nodes {C:\post-install\Clear-SdsConfig.ps1 -confirm:$False}
-Enter-PSSession -ComputerName S2D2
+#Invoke-Command $Nodes {C:\post-install\Clear-SdsConfig.ps1 -confirm:$False}
+#Enter-PSSession -ComputerName S2D2
 
 Invoke-Command $Nodes {
 #DO NOT RUN THIS COMMAND IN PROD!!!
@@ -11,3 +11,16 @@ Get-VmSwitch | Where-Object {$_.Name -ne 'Embedded_vSwitch_Team_Production'} | R
 Get-NetQosPolicy | Remove-NetQosPolicy -confirm:$Falase
 Get-NetQosTrafficClass | Remove-NetQosTrafficClass
 #Clear-ClusterNode
+
+
+
+
+}
+
+#Invoke-command $Nodes {Clear-ClusterNode}
+
+Invoke-command $Nodes {Get-PhysicalDisk | ft}
+
+Invoke-command $Nodes {Get-StorageSubSystem | ft}
+
+#endregion
