ab36364ab43c1ed882068e84c7884cb40ed73b81
dkawula <dkawula@triconelite.com>
2018-09-13 18:17:09 -0600

    Script to Setup Forced Tunneling for Azure
    This is required to force traffic back through the on prem VPN Gateway
    vs. local to Azure

---
+++ Azure/ConfigureForceTunneling.ps1
@@ -0,0 +1,13 @@
+﻿Install-Module -Name AzureRM
+Connect-AzureRMAccount
+Get-AzureRMResourceGroup
+Get-AzureRMVirtualnetworkGateway -ResourceGroupName tc-az-dr03
+Get-AzureRMLocalNetworkGateway -ResourceGroupName tc-az-dr03
+
+
+$LocalGateway = Get-AzureRmLocalNetworkGateway -Name "TriconElite-HQ" -ResourceGroupName "tc-az-dr03"
+$LocalGateway
+$VirtualGateway = Get-AzureRmVirtualNetworkGateway -Name "tc-az-dr03-vnet1-GW" -ResourceGroupName "tc-az-dr03"
+$VirtualGateway
+
+Set-AzureRmVirtualNetworkGatewayDefaultSite -GatewayDefaultSite $LocalGateway -VirtualNetworkGateway $VirtualGateway
