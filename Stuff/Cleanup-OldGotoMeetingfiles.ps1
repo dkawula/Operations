bf2033e3ab2a8460896cedf8110c6ce33e77eabc
dkawula <dkawula@triconelite.com>
2019-09-17 12:43:06 -0600

    Cleanup GotoMeeting Crap

---
+++ Stuff/Cleanup-OldGotoMeetingfiles.ps1
@@ -0,0 +1,7 @@
+﻿#Remove Scrap Goto Meeting Files
+
+Remove-Item –path C:\users\DKTCLAPTOP\Documents\original\ -include *.g2m –recurse
+
+#Remove all of the older installer files that Goto Meeting Leaves Behind 60 days or older
+
+Get-Childitem "C:\users\DKTCLAPTOP\AppData\local\GoToMeeting" | Where {$_.CreationTime -lt (get-date).adddays(-45)} | Remove-Item -Recurse
