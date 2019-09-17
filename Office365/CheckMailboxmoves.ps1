c9855624aafe50f0cba70ab3dc896f904911fe37
dkawula <dkawula@triconelite.com>
2019-09-17 12:42:55 -0600

    Checking on Office365 Mailbox Moves

---
+++ Office365/CheckMailboxmoves.ps1
@@ -0,0 +1,8 @@
+#Connect to Office 365
+$usercredential = get-credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+Import-PSSession $Session
+
+Get-MoveRequest | where {$_.status -notlike “complete*”} | Get-MoveRequestStatistics | Select DisplayName,status,percentcomplete,itemstransferred
+
+Get-MoveRequest 
