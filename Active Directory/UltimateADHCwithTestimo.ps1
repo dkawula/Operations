432550ffaab5e6e1a2e4f16753e5cd6763303d03
dkawula <dkawula@triconelite.com>
2019-12-05 16:08:45 -0700

    Best AD Health check Ever

---
+++ Active Directory/UltimateADHCwithTestimo.ps1
@@ -0,0 +1,37 @@
+﻿Install-Module Testimo -Force -Verbose
+
+$Sources = @(
+
+    'ForestRoles'
+
+    'ForestOptionalFeatures'
+
+    'ForestOrphanedAdmins'
+
+    'DomainPasswordComplexity'
+
+    'DomainKerberosAccountAge'
+
+    'DomainDNSScavengingForPrimaryDNSServer'
+
+    'DomainSysVolDFSR'
+
+    'DCRDPSecurity'
+
+    'DCSMBShares'
+
+    'DomainGroupPolicyMissingPermissions'
+
+    'DCWindowsRolesAndFeatures'
+
+    'DCNTDSParameters'
+
+    'DCInformation'
+
+    'ForestReplicationStatus'
+
+)
+
+
+
+Invoke-Testimo -Sources $Sources -ShowReport
