e22fb3eb5229756917611ebab84f103e03424921
dkawula <dkawula@triconelite.com>
2019-12-04 17:25:21 -0700

    Script to Start Wan Accelerators
    This can be run as a scheduled task --> Just a quick an dirty way to
    grab the Wan Accelerators that seem to keep having the Veeam Service
    Fail and force start them so the remote jobs will work.

---
+++ Veeam/Veeam-RestartWanAccelServices.ps1
@@ -0,0 +1,24 @@
+﻿asnp "VeeamPSSnapIn" -ErrorAction SilentlyContinue
+
+$vbwans = Get-VBRWANAccelerator
+$vbwans1 = $vbwans.name
+$vbwans1
+
+Foreach ($VBWAN in $vbwans1) {
+
+TRY{
+invoke-command -ComputerName $VBWAN {
+Write-Host "Starting Veeam Services on:"$VBWAN
+Get-Service -Name Veeam* | Start-Service -verbose
+
+}
+}
+
+Catch {
+
+"Error Contacting Server: $VBWAN"
+
+
+}
+
+}
