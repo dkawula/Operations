a06397d87c415aa315049c7ebc0c9a120f52e734
dkawula <dkawula@triconelite.com>
2021-01-05 12:24:23 -0700

    Script to Fix 50 GB quota limit for E3 License should be 100 GB
    Microsoft increased the Quota limit for E3 Licenses from 50 to 100 GB at
    some point but it didn't update in my customer tenant.   Note:  You will
    likely need to get Microsoft to run a resync on your tenant post running
    this that is what I needed to do.   Simply cut a ticket with them after
    you set this in the Office 365 Admin Portal.

---
+++ Office365/ExchangeOnline-Fix50GBQuotato100GB.ps1
@@ -0,0 +1,23 @@
+﻿#fix for users getting up to full 10 GB on E3 License
+#We noticed a bug that had them set at a 50 GB quota
+Set-ExecutionPolicy RemoteSigned [a for all]
+Import-Module ExchangeOnlineManagement
+#Connection that works for an account with MFA
+Connect-ExchangeOnline -UserPrincipalName dkawula@customer.com -ShowProgress $true 
+Install-Module -Name AzureAD
+
+get-Mailbox dkawula@customer.com | Format-List IssueWarningQuota,ProhibitSendQuota,ProhibitSendReceiveQuota,UseDatabaseQuotaDefaults
+
+Set-Mailbox -Identity "dkawula@tcustomer.com" -IssueWarningQuota 98gb -ProhibitSendQuota 98gb -ProhibitSendReceiveQuota 99gb -UseDatabaseQuotaDefaults $false -verbose
+
+get-Mailbox dkawula@customer.com | Format-List IssueWarningQuota,ProhibitSendQuota,ProhibitSendReceiveQuota,UseDatabaseQuotaDefaults
+
+#after this verify that your quota looks something like this
+#IssueWarningQuota        : 98 GB (105,226,698,752 bytes)
+#ProhibitSendQuota        : 98 GB (105,226,698,752 bytes)
+#ProhibitSendReceiveQuota : 99 GB (106,300,440,576 bytes)
+#UseDatabaseQuotaDefaults : False
+
+
+#NOTE:  I had to cut a ticket with MSFT Support to get them to run a backend sync on my mailboxes because the quotas were not propogating to their front end farms.
+#After this was done it only took an hour and it was good.
