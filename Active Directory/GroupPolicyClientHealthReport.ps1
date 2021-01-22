f21f94c5d0d1dabf9bc9aab9d736b5addb2bf453
dkawula <dkawula@triconelite.com>
2021-01-22 13:00:35 -0700

    Script to run a simple report on GPO Client Health

---
+++ Active Directory/GroupPolicyClientHealthReport.ps1
@@ -0,0 +1,25 @@
+﻿function Get-ClientGPOReport {
+
+param(
+    [string] $computer = "localhost",
+    [int]    $hours    = 5
+    
+)
+
+#Get date relative to the number of hours you want to review
+$hours = '5'
+$computer = 'localhost'
+$date = (Get-Date).Addhours(-$($hours))
+
+
+#load the name of the Winevent log corresponding to Group Policy
+$loggpo= "Microsoft-Windows-GroupPolicy/Operational"
+
+$GroupPolicyClientEvents = Get-WinEvent -FilterHashtable @{ LogName = $loggpo ; StartTime = $date } -ComputerName $computer -Oldest
+$GroupPolicyClientEvents
+   }
+
+
+Install-Module -Name PSWriteHTML -AllowClobber -Force
+
+Get-ClientGPOReport -hours 5 | Out-HtmlView
