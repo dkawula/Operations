17a5c966ca7a76b6229a6e1240a5c7c8d323def1
Dave Kawula - MVP <dkawula@triconelite.com>
2021-05-28 11:06:57 -0600

    Create Prod_Nic_MGMT.ps1

---
+++ Titan_Fabric/Prod_Nic_MGMT.ps1
@@ -0,0 +1,22 @@
+﻿#PowerShell Script to Disable Internet Adapters 
+#You need to know the name of the Adapter - In the example vEthernet (MGMT)
+
+#Make an Array of Servers
+
+$FabricServers = "Kender-S2D1","Server2","Server3"
+
+#Test the Array
+
+$FabricServers
+
+#Get the Netadapter and Enable them when ready for patching
+
+#Get-NetAdapter -Name "Ethernet 13" | Enable-NetAdapter -Verbose
+
+#Get the Netadapter and Disable it when done patching
+
+Get-NetAdapter -Name "Ethernet 13" | Disable-NetAdapter -Verbose -confirm:$false
+
+#Get the Netadapter and cycle it restart
+
+#Get-NetAdapter -Name "Ethernet 13" | Restart-NetAdapter -Verbose
