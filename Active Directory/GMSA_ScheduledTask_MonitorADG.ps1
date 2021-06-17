d423ffddb44c7de69257f2c92ecd03daa8838a19
Dave Kawula - MVP <dkawula@triconelite.com>
2021-06-17 11:58:37 -0600

    Monitor ADG

---
+++ Active Directory/GMSA_ScheduledTask_MonitorADG.ps1
@@ -0,0 +1,9 @@
+﻿#Steps to Register a GMSA as a scheduled task
+
+$action = New-ScheduledTaskAction powershell.exe -Argument "-file `"c:\post-install\005-MonitorADG\Monitor-ADGroupMemberShip.ps1 -group \"Domain Admins\",\"Schema Admins\",\"Enterprise Admins\" -htmllog`""
+$trigger = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes 3)
+$principal = New-ScheduledTaskPrincipal -UserID CYL\MonitorADG$ -LogonType Password
+Register-ScheduledTask MonitorADG –Action $action –Trigger $trigger –Principal $principal
+
+Set-ScheduledTask -TaskName MonitorADG -Principal $principal
+
