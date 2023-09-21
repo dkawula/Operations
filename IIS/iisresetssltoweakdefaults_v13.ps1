be1cfecbeaf3910211fbd50c7376c7e9962db988
Dave Kawula - MVP <dkawula@triconelite.com>
2023-09-21 09:40:38 -0600

    IIS Hardening

---
+++ IIS/iisresetssltoweakdefaults_v13.ps1
@@ -0,0 +1,53 @@
+﻿# Copyright 2019, Alexander Hass
+# http://www.hass.de/content/setup-your-iis-ssl-perfect-forward-secrecy-and-tls-12
+#
+# Version 1.3
+# - Rollback script created.
+ 
+Write-Host 'Reset IIS to weak and insecure SSL defaults...'
+Write-Host '--------------------------------------------------------------------------------'
+
+New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers' -Force
+New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\CipherSuites' -Force
+New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Hashes' -Force
+New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\KeyExchangeAlgorithms' -Force
+New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols' -Force
+
+$os = Get-WmiObject -class Win32_OperatingSystem
+if ([System.Version]$os.Version -lt [System.Version]'10.0.17763') {
+  # Less than Windows 2019
+  New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Client' -Force
+  New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Client' -name DisabledByDefault -value 1 -PropertyType 'DWord'
+}
+
+New-Item 'HKLM:\SOFTWARE\Policies\Microsoft\Cryptography\Configuration\SSL\00010002' -Force
+
+Write-Host 'Reset .NET to weak and insecure SSL defaults...'
+Remove-ItemProperty -path "HKLM:\SOFTWARE\Microsoft\.NETFramework\v2.0.50727" -name 'SystemDefaultTlsVersions' -Force
+Remove-ItemProperty -path "HKLM:\SOFTWARE\Microsoft\.NETFramework\v2.0.50727" -name 'SchUseStrongCrypto' -Force
+Remove-ItemProperty -path "HKLM:\SOFTWARE\Microsoft\.NETFramework\v4.0.30319" -name 'SystemDefaultTlsVersions' -Force
+Remove-ItemProperty -path "HKLM:\SOFTWARE\Microsoft\.NETFramework\v4.0.30319" -name 'SchUseStrongCrypto' -Force
+if (Test-Path 'HKLM:\SOFTWARE\Wow6432Node') {
+  Remove-ItemProperty -path "HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NETFramework\v2.0.50727" -name 'SystemDefaultTlsVersions' -Force
+  Remove-ItemProperty -path "HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NETFramework\v2.0.50727" -name 'SchUseStrongCrypto' -Force
+  Remove-ItemProperty -path "HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NETFramework\v4.0.30319" -name 'SystemDefaultTlsVersions' -Force
+  Remove-ItemProperty -path "HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NETFramework\v4.0.30319" -name 'SchUseStrongCrypto' -Force
+}
+
+Write-Host 'Reset WinHttp to weak and insecure SSL defaults...'
+Remove-ItemProperty -path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\WinHttp' -name 'DefaultSecureProtocols' -Force
+if (Test-Path 'HKLM:\SOFTWARE\Wow6432Node') {
+  if ([System.Version]$os.Version -lt [System.Version]'10.0.17763') {
+    Remove-ItemProperty -path 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Internet Settings\WinHttp' -name 'DefaultSecureProtocols' -Force
+  } else {
+    # Windows 2019
+    Remove-Item 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Internet Settings\WinHttp' -Force
+  }
+}
+
+Write-Host 'Reset Windows Internet Explorer to weak and insecure SSL defaults...'
+Remove-ItemProperty -path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' -name 'SecureProtocols' -Force
+Remove-ItemProperty -path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings' -name 'SecureProtocols' -Force
+
+
+Restart-Computer -Force
