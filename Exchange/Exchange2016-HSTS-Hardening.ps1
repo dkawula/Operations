837f3597c7bc11a0f0720ac047751dbfb8f624a5
Dave Kawula - MVP <dkawula@triconelite.com>
2023-10-12 10:08:47 -0600

    Create Exchange2016-HSTS-Hardening.ps1
    upload

---
+++ Exchange/Exchange2016-HSTS-Hardening.ps1
@@ -0,0 +1,14 @@
+﻿#Harden Exchange Server 2016 with HSTS
+
+Import-Module IISAdministration
+Reset-IISServerManager -Confirm:$false
+Start-IISCommitDelay
+
+$iisConfig = Get-IISConfigSection -SectionPath "system.webServer/httpProtocol" -CommitPath "Default Web Site" | Get-IISConfigCollection -CollectionName "customHeaders"
+New-IISConfigCollectionElement -ConfigCollection $iisConfig -ConfigAttribute @{"name"="Strict-Transport-Security"; "value"="max-age=31536000; includeSubDomains";} #MaxAge 31536000 = 1 year
+
+
+
+
+Stop-IISCommitDelay
+Remove-Module IISAdministration
