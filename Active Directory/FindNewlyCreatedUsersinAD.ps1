3d35d7c12eac0408459ee1d5f52e69f74333aca1
Dave Kawula - MVP <dkawula@triconelite.com>
2024-10-04 10:51:48 -0600

    Create FindNewlyCreatedUsersinAD.ps1

---
+++ Active Directory/FindNewlyCreatedUsersinAD.ps1
@@ -0,0 +1,16 @@
+﻿# Define the number of days to search for newly created users
+$daysBack = 30 # Change this to the number of days you want to search for
+
+# Get the current date and subtract the number of days to find new users
+$startDate = (Get-Date).AddDays(-$daysBack)
+
+# Import Active Directory module (if needed)
+Import-Module ActiveDirectory
+
+# Search for users created in the last X days
+$newUsers = Get-ADUser -Filter {WhenCreated -gt $startDate} -Properties WhenCreated, DisplayName | 
+    Select-Object DisplayName, SamAccountName, WhenCreated
+
+# Display the results
+$newUsers | Format-Table -AutoSize
+$newusers
