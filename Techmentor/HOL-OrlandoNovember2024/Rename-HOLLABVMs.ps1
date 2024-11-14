a4fa3ba1c7dccd97ea00b269cdfcd9a259034798
Dave Kawula - MVP <dkawula@triconelite.com>
2024-11-13 21:01:25 -0700

    Update Rename-HOLLABVMs.ps1

--- Techmentor/HOL-OrlandoNovember2024/Rename-HOLLABVMs.ps1
+++ Techmentor/HOL-OrlandoNovember2024/Rename-HOLLABVMs.ps1
@@ -1,53 +1,18 @@
-﻿#Rename Lab Computers
-#This script will renname the lab computers and help with the Azure Hybrid and Azure Arc Labs
-#Dave Kawula - MVP
+﻿$startIP = 31
+$endIP = 67
+$startStudentNumber = 24
 
-# Define the starting IP and starting student number
-$startIP = "192.168.200.11"
-$endIP = "192.168.200.67"
-$startStudentNumber = 1
+for ($i = $startIP; $i -le $endIP; $i++) {
+    if ($i -eq 57) { continue }  # Skip IP 192.168.200.57
 
-# Convert IP address to an array of integers
-function Convert-IPToArray($ipAddress) {
-    return $ipAddress.Split(".") | ForEach-Object { [int]$_ }
-}
+    $ip = "192.168.200.$i"
+    $newComputerName = "Student$startStudentNumber"
 
-# Convert array of integers back to IP address string
-function Convert-ArrayToIP($ipArray) {
-    return ($ipArray -join ".")
-}
+    Invoke-Command -ComputerName $ip -ScriptBlock {
+        param ($newName)
+        Rename-Computer -NewName $newName -Restart -Force -Verbose
+    } -ArgumentList $newComputerName
 
-# Increment an IP address by 1
-function Increment-IP($ipArray) {
-    for ($i = 3; $i -ge 0; $i--) {
-        if ($ipArray[$i] -lt 255) {
-            $ipArray[$i]++
-            break
-        } else {
-            $ipArray[$i] = 0
-        }
-    }
-    return $ipArray
-}
-
-# Convert starting and ending IPs
-$currentIPArray = Convert-IPToArray $startIP
-$endIPArray = Convert-IPToArray $endIP
-
-# Loop through each IP from start to end
-$currentStudentNumber = $startStudentNumber
-
-while ($true) {
-    $currentIP = Convert-ArrayToIP $currentIPArray
-    $newComputerName = "Student{0:D2}" -f $currentStudentNumber
-
-    # Rename the computer
-    Rename-Computer -ComputerName $currentIP -NewName $newComputerName -Restart -Force -verbose
-
-    # Increment IP and student number
-    $currentIPArray = Increment-IP $currentIPArray
-    $currentStudentNumber++
-
-    # Stop if we've reached the end IP
-    if ($currentIPArray -eq $endIPArray) { break }
+    # Increment the student number for the next iteration
+    $startStudentNumber++
 }
