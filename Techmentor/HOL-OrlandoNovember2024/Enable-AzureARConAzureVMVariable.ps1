c27d44c5ef85ab39f0c3a3a2eedb8ee8772f524a
Dave Kawula - MVP <dkawula@triconelite.com>
2024-11-13 21:10:58 -0700

    Update Enable-AzureARConAzureVMVariable.ps1

--- Techmentor/HOL-OrlandoNovember2024/Enable-AzureARConAzureVMVariable.ps1
+++ Techmentor/HOL-OrlandoNovember2024/Enable-AzureARConAzureVMVariable.ps1
@@ -3,9 +3,12 @@
 #Dave Kawula -MVP
 
 
-for ($i = 11; $i -le 67; $i++) {
-    $ip = "192.168.200.$i"
-    Invoke-Command -ComputerName $ip -ScriptBlock {
+# Define the range of student names
+$studentNames = 1..60 | ForEach-Object { "Student{0:D2}" -f $_ }
+
+# Loop through each student name and set the environment variable
+foreach ($student in $studentNames) {
+    Invoke-Command -ComputerName $student -ScriptBlock {
         [System.Environment]::SetEnvironmentVariable("MSFT_ARC_TEST", 'true', [System.EnvironmentVariableTarget]::Machine)
-    }
+    } 
 }
