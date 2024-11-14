dca27f160037c7c5fad75ca1ddfb88e2786d9a69
Dave Kawula - MVP <dkawula@triconelite.com>
2024-11-13 20:11:57 -0700

    Create Enable-AzureARConAzureVMVariable.ps1

---
+++ Techmentor/HOL-OrlandoNovember2024/Enable-AzureARConAzureVMVariable.ps1
@@ -0,0 +1,11 @@
+﻿#Enable Azure Arc Install Variable on Azure Lab VM's
+#This script will prime the lab VM's so that students can possibly install Azure Arc
+#Dave Kawula -MVP
+
+
+for ($i = 11; $i -le 67; $i++) {
+    $ip = "192.168.200.$i"
+    Invoke-Command -ComputerName $ip -ScriptBlock {
+        [System.Environment]::SetEnvironmentVariable("MSFT_ARC_TEST", 'true', [System.EnvironmentVariableTarget]::Machine)
+    }
+}
