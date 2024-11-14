80c7310773a1ca272dd22e21e1d52deb89e70f43
Dave Kawula - MVP <dkawula@triconelite.com>
2024-11-13 19:38:59 -0700

    1

--- Techmentor/HOL-OrlandoNovember2024/Create-HOLLABVMS_Azure.ps1
+++ Techmentor/HOL-OrlandoNovember2024/Create-HOLLABVMS_Azure.ps1
@@ -6,7 +6,7 @@
 # Created by Dave Kawula - MVP
 $resourceGroup = "TMHOLMIGSRV2025"
 $location = "Canada Central" 
-$imageId = "/subscriptions/3bc59d7d-b822-4d55-bc1d-24c95ad5d3a5/resourceGroups/<ResourceGroup>/providers/Microsoft.Compute/galleries/HoLGallery/images/TechmentorOrlando2024-HOL2/versions/2.1.0"
+$imageId = "/subscriptions/<InsertSub>/resourceGroups/<ResourceGroup>/providers/Microsoft.Compute/galleries/HoLGallery/images/TechmentorOrlando2024-HOL2/versions/2.1.0"
 $sku = "Standard_D8s_v3" 
 $vmPrefix = "Student"
 $vnetName = "TMLABVNET04"
@@ -34,7 +34,7 @@ if (-not (Get-AzNetworkSecurityGroup -Name $nsgName -ResourceGroupName $resource
 }
 
 # Loop to create 50 VMs with unique names
-for ($i = 1; $i -le 50; $i++) {
+for ($i = 51; $i -le 60; $i++) {
     # Format the VM name as StudentXX (e.g., Student01, Student02)
     $vmName = "{0}{1:D2}" -f $vmPrefix, $i
 
---
+++ Techmentor/HOL-OrlandoNovember2024/Install-AzureArcAzureVM.ps1
@@ -0,0 +1,56 @@
+﻿#Install Arc on Azure VM for Techmentor HOL
+#Dave Kawula -MVP
+
+[System.Environment]::SetEnvironmentVariable("MSFT_ARC_TEST",'true', [System.EnvironmentVariableTarget]::Machine)
+
+#https://learn.microsoft.com/en-us/azure/azure-arc/servers/plan-evaluate-on-azure-virtual-machine
+
+#Manual Deployment of Azure Arc to LAB Host VM running in Azure
+
+$global:scriptPath = $myinvocation.mycommand.definition
+
+function Restart-AsAdmin {
+    $pwshCommand = "powershell"
+    if ($PSVersionTable.PSVersion.Major -ge 6) {
+        $pwshCommand = "pwsh"
+    }
+
+    try {
+        Write-Host "This script requires administrator permissions to install the Azure Connected Machine Agent. Attempting to restart script with elevated permissions..."
+        $arguments = "-NoExit -Command `"& '$scriptPath'`""
+        Start-Process $pwshCommand -Verb runAs -ArgumentList $arguments
+        exit 0
+    } catch {
+        throw "Failed to elevate permissions. Please run this script as Administrator."
+    }
+}
+
+try {
+    if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+        if ([System.Environment]::UserInteractive) {
+            Restart-AsAdmin
+        } else {
+            throw "This script requires administrator permissions to install the Azure Connected Machine Agent. Please run this script as Administrator."
+        }
+}
+
+    $env:SUBSCRIPTION_ID = "62bf0c70-c205-46d4-b220-37b2e262c9e9";
+    $env:RESOURCE_GROUP = "rgtmlab";
+    $env:TENANT_ID = "24de82e8-d5f5-4bf7-9aa8-91f573f12ace";
+    $env:LOCATION = "canadacentral";
+    $env:AUTH_TYPE = "token";
+    $env:CORRELATION_ID = "686c1dd0-1ce9-4a78-8701-56b9bc871f47";
+    $env:CLOUD = "AzureCloud";
+    
+
+    [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor 3072;
+    Invoke-WebRequest -UseBasicParsing -Uri "https://gbl.his.arc.azure.com/azcmagent-windows" -TimeoutSec 30 -OutFile "$env:TEMP\install_windows_azcmagent.ps1";
+    & "$env:TEMP\install_windows_azcmagent.ps1";
+    if ($LASTEXITCODE -ne 0) { exit 1; }
+    & "$env:ProgramW6432\AzureConnectedMachineAgent\azcmagent.exe" connect --resource-group "$env:RESOURCE_GROUP" --tenant-id "$env:TENANT_ID" --location "$env:LOCATION" --subscription-id "$env:SUBSCRIPTION_ID" --cloud "$env:CLOUD" --tags "ArcSQLServerExtensionDeployment=Disabled" --correlation-id "$env:CORRELATION_ID";
+}
+catch {
+    $logBody = @{subscriptionId="$env:SUBSCRIPTION_ID";resourceGroup="$env:RESOURCE_GROUP";tenantId="$env:TENANT_ID";location="$env:LOCATION";correlationId="$env:CORRELATION_ID";authType="$env:AUTH_TYPE";operation="onboarding";messageType=$_.FullyQualifiedErrorId;message="$_";};
+    Invoke-WebRequest -UseBasicParsing -Uri "https://gbl.his.arc.azure.com/log" -Method "PUT" -Body ($logBody | ConvertTo-Json) | out-null;
+    Write-Host  -ForegroundColor red $_.Exception;
+}
